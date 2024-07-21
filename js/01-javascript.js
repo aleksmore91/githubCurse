ca1900c04b97c3b53c775fc6ab8c9350427d5f71
aleksmore91 <aleksmore91@gmail.com>
2024-07-21 17:12:47 -0600

    Primer commit

---
+++ js/01-javascript.js
@@ -0,0 +1,23 @@
+
+/* -------------VARIABLES-------------- */
+
+let nombre = "Uriel";
+let apellido = "Moreno";
+let nombreApellido = nombre + " " + apellido;
+
+let anioActual = 2024;
+let anioNacimiento = 1991;
+let edad = anioActual - anioNacimiento;
+
+/* -------------CONSTANTES------------ */
+
+const IVA = 16;
+
+
+console.log(edad);
+
+
+
+let nombreUsuario = prompt("ingresar nombre");
+
+console.log(nombreUsuario);
