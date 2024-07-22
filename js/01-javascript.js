29f3a3f113612e623faeda95cf215f9f52086412
aleksmore91 <aleksmore91@gmail.com>
2024-07-21 20:34:22 -0600

    actualizacion archivo js

--- js/01-javascript.js
+++ js/01-javascript.js
@@ -1,5 +1,5 @@
 
-/* -------------VARIABLES-------------- */
+/* -------------VARIABLES-------------- 
 
 let nombre = "Uriel";
 let apellido = "Moreno";
@@ -9,15 +9,39 @@ let anioActual = 2024;
 let anioNacimiento = 1991;
 let edad = anioActual - anioNacimiento;
 
-/* -------------CONSTANTES------------ */
+/* ------------- CONSTANTES ---------------- 
 
 const IVA = 16;
-
 
 console.log(edad);
 
 
+/* ---------  PROMPT ventana emergente ------------ 
 
 let nombreUsuario = prompt("ingresar nombre");
 
-console.log(nombreUsuario);
+console.log(nombreUsuario); 
+
+
+// -----------  CONDICIONALES Y OPERADORES LÓGICOS ---------  */
+
+// ==           Es igual
+// ===          Es estrictamente igual
+// !=           Es distinto
+// !==          Es estrictamente distinto
+// < <= > >=    Menor / Mayor / Menor o igual / Mayor o igual
+
+// &&           Operador AND (y)
+// ||           Operador OR (o)
+// !            Operador NOT (no)
+
+
+let edad = prompt("ingresar edad");
+
+if (edad >= 18) {
+    alert("Eres mayor de edad, puedes ingresar");
+} else {
+    alert("Eres menor de edad, no puedes ingresar");
+}
+
+
